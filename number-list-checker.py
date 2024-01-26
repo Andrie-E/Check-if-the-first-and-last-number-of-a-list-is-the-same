9e631e3796c15aeaff17d30993ffbdba2bfa6c88
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 16:42:21 +0800

    code for pseudocode 1,2, and 3

--- number-list-checker.py
+++ number-list-checker.py
@@ -3,6 +3,13 @@
 # Pseudocode
 
 # Create the function
+def is_first_and_last_same(numbers):
+    return len(numbers) > 0 and numbers[0] == numbers[-1]
 # Provide the list
+numbers_x = [10, 20, 30, 40, 10]
+numbers_y = [75, 65, 35, 75, 30]
 # Test the function
+result_x = is_first_and_last_same(numbers_x)
+result_y = is_first_and_last_same(numbers_y)
+print(result_x)
 # Print
