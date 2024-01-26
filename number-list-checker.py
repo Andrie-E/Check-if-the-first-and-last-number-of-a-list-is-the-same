085246b68642479daa89d5ea77405e6e34aa5f3a
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 16:35:40 +0800

    pseudocode

--- number-list-checker.py
+++ number-list-checker.py
@@ -1,1 +1,8 @@
 # Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
+
+# Pseudocode
+
+# Create the function
+# Provide the list
+# Test the function
+# Print
